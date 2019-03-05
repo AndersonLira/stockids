a82f2019454cf85de8696d45562fcef7d73a8e31
Anderson Lira <andersonliralink@gmail.com>
2019-03-05 18:55:27 +0000

    First commit. Not functional. Not works

---
+++ lambdas/db.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+)
+
+func GetDB() *dynamodb.DynamoDB {
+	sess, err := session.NewSession(&aws.Config{
+		Region: aws.String("us-east-1")},
+	)
+
+	if err != nil {
+		fmt.Println("Error creating session:")
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	// Create DynamoDB client
+	svc := dynamodb.New(sess)
+	return svc
+}

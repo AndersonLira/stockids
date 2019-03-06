002a1f453d7edf05118fd588c802ffc924797d0d
Anderson Lira <andersonliralink@gmail.com>
2019-03-06 21:22:04 +0000

    Dep initialized

--- lambdas/children/get.go
+++ lambdas/children/get.go
@@ -3,10 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"os"
 
-	"github.com/andersonlira/geneucash/accounts/model"
-	"github.com/andersonlira/goutils/str"
 	"github.com/andersonlira/stockids/db"
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-sdk-go/aws"
@@ -14,45 +11,45 @@ import (
 	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 )
 
-func Create(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	svc := GetDB()
+// func Create(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+// 	svc := GetDB()
 
-	items := []model.Account{model.Account{ID: str.NewUUID(), Name: "Account 1"}}
+// 	items := []model.Account{model.Account{ID: str.NewUUID(), Name: "Account 1"}}
 
-	// Add each item to Movies table:
-	for _, item := range items {
-		av, err := dynamodbattribute.MarshalMap(item)
+// 	// Add each item to Movies table:
+// 	for _, item := range items {
+// 		av, err := dynamodbattribute.MarshalMap(item)
 
-		if err != nil {
-			fmt.Println("Got error marshalling map:")
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+// 		if err != nil {
+// 			fmt.Println("Got error marshalling map:")
+// 			fmt.Println(err.Error())
+// 			os.Exit(1)
+// 		}
 
-		// Create item in table Movies
-		input := &dynamodb.PutItemInput{
-			Item:      av,
-			TableName: aws.String("accounts"),
-		}
+// 		// Create item in table Movies
+// 		input := &dynamodb.PutItemInput{
+// 			Item:      av,
+// 			TableName: aws.String("accounts"),
+// 		}
 
-		_, err = svc.PutItem(input)
+// 		_, err = svc.PutItem(input)
 
-		if err != nil {
-			fmt.Println("Got error calling PutItem:")
-			fmt.Println(err.Error())
-			os.Exit(1)
-		}
+// 		if err != nil {
+// 			fmt.Println("Got error calling PutItem:")
+// 			fmt.Println(err.Error())
+// 			os.Exit(1)
+// 		}
 
-		fmt.Println("Successfully added '", item.Name)
-	}
+// 		fmt.Println("Successfully added '", item.Name)
+// 	}
 
-	bk := &message{
-		Title:   "Message",
-		Message: "Showing message",
-	}
-	response, _ := json.Marshal(bk)
-	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 201}, nil
-}
+// 	bk := &message{
+// 		Title:   "Message",
+// 		Message: "Showing message",
+// 	}
+// 	response, _ := json.Marshal(bk)
+// 	return events.APIGatewayProxyResponse{Body: string(response), StatusCode: 201}, nil
+// }
 
 //GetChildren returns childrens from giving parent
 func GetChildren(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {

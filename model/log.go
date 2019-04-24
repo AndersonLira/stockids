6a5c305212dedce1dff153599f462187705dad9d
Anderson Lira <andersonliralink@gmail.com>
2019-04-24 09:35:12 +0100

    family model adjusted. Log recovered

--- model/log.go
+++ model/log.go
@@ -1,7 +1,7 @@
 package model
 
-//Family represents one group
-type Family struct {
+//Log register each behaviour of child
+type Log struct {
 	ChildID string `json:"child_id"`
 	Date    int64  `json:"date"`
 	Message string `json:"message"`
